41e1757a51054f62677483a3ed3faed2644c152e
Antonio Ariza Gomez <luiari@outlook.com>
2023-05-29 22:51:42 +0200

    update

--- SquidShopApi/Controllers/PromotionsController.cs
+++ SquidShopApi/Controllers/PromotionsController.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
+﻿using System.Net;
 using AutoMapper;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using SquidShopApi.Data;
 using SquidShopApi.Models;
 using SquidShopApi.Models.DTO;
-using SquidShopApi.Repository;
 using SquidShopApi.Repository.IRepository;
 
 
