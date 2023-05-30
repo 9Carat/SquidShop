f78a7cdd457540f6c2ef601769f4b04b20b1a026
Christoffer Ottestig <98085034+9Carat@users.noreply.github.com>
2023-05-30 10:11:11 +0200

    Order/OrderList + detailed orderview added

---
+++ SquidShopWebApp/Models/DTO/OrderListCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SquidShopWebApp.Models.DTO
+{
+    public class OrderListCreateDTO
+    {
+        public int OrderListId { get; set; }
+        public int FK_ProductId { get; set; }
+        public int Fk_OrderId { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ SquidShopWebApp/Services/IServices/IOrderListService.cs
@@ -0,0 +1,13 @@
+﻿using SquidShopWebApp.Models.DTO;
+
+namespace SquidShopWebApp.Services.IServices
+{
+    public interface IOrderListService
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetByIdAsync<T>(int id);
+        Task<T> CreateAsync<T>(OrderListCreateDTO dto);
+        Task<T> UpdateAsync<T>(OrderListUpdateDTO dto);
+        Task<T> DeleteAsync<T>(int id);
+    }
+}
