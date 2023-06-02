784aa71c84044c58fc3b6dfca29896dee8a1dfd4
niklassendelbach <niklas@sendelbach.se>
2023-06-02 14:59:29 +0200

    small text fix

--- SquidShopWebApp/Models/Product.cs
+++ SquidShopWebApp/Models/Product.cs
@@ -8,17 +8,22 @@ namespace SquidShopWebApp.Models
 	{
         public int ProductId { get; set; }
         [StringLength(25)]
+        [DisplayName("Product name")]
         public string ProductName { get; set; }
+        [DisplayName("In stock")]
         public int InStock { get; set; }
+        [DisplayName("Unit price")]
         public double UnitPrice { get; set; }
         public decimal Discount { get; set; }
+        [DisplayName("Price after discount")]
         public double DiscountUnitPrice { get; set; }
         [StringLength(75)]
-        [DisplayName("Image Name")]
+        [DisplayName("Image name")]
         public string ImageName { get; set; }
         [NotMapped]
         [DisplayName("Upload Image")]
         public IFormFile ImageFile { get; set; }
+        [DisplayName("Category")]
         public int FK_CategoryId { get; set; }
         public Category Categories { get; set; }
         public ICollection<OrderList> OrderLists { get; set; }
